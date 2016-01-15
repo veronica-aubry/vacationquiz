94c5d33c19ee993634751524f5691f7137878a01
Veronica Alley <veronica.aubry.alley@gmail.com>
2016-01-15 10:29:29 -0800

    add new result

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,54 @@
 $(document).ready(function() {
 
-$("").click(function() {
-  $("#picture1").show();
+  $("#form").submit(function(event) {
 
-});
+    $("#beachResult").hide();
+    $("#shackResult").hide();
+    $("#forestTempleResult").hide();
 
+
+    var activity = $("input[name=question1]:checked").val();
+    var describe = $("input[name=question2]:checked").val();
+    var father = $("input[name=question3]:checked").val();
+    var dream = $("input[name=question4]:checked").val();
+    var take = $("input[name=question5]:checked").val();
+
+    //temple results//
+
+    if ( activity === "activityAdventuring" && describe === "describeOffbeat") {
+      $("#forestTempleResult").show()
+    } else if ( activity === "activityAdventuring" && describe === "describeContent" && father === "father1" && take !== "take1") {
+      $("#forestTempleResult").show()
+
+    //beach results//
+
+    } else if ( activity === "activityCocktails" && describe === "describeContent") {
+        $("#beachResult").show()
+
+    } else if ( activity === "activityCocktails" && describe === "describeTired" && take === "take1") {
+      $("#beachResult").show()
+
+    } else if ( activity === "activityAdventuring" && describe === "describeContent" && take === "take1") {
+      $("#beachResult").show()
+
+    //apartment results//
+
+  } else if ( activity === "activityCocktails" && describe === "describeOffbeat") {
+      $("#apartmentResult").show()
+
+    } else if ( activity === "activityCocktails" && describe === "describeTired" && dream === "dreamVoid") {
+      $("#apartmentTempleResult").show()
+
+    } else if ( activity === "activityAdventuring" && describe === "describeContent" && take === "take1") {
+      $("#apartmentResult").show()
+
+
+
+
+  } else {
+    $("#shackResult").show()
+    }
+
+    event.preventDefault();
+  });
 });
