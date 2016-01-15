a671ed26a4f02b5aabcf6f3833ff3fa27e78927d
Veronica Alley <veronica.aubry.alley@gmail.com>
2016-01-15 12:43:18 -0800

    change design of quiz layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
 
+  $("#toQuestion2").click(function(event) {
+    $("#question1").hide();
+    $("#question2").show();
+
+  });
+
+  $("#toQuestion3").click(function(event) {
+    $("#question2").hide();
+    $("#question3").show();
+
+  });
+
+  $("#toQuestion4").click(function(event) {
+    $("#question3").hide();
+    $("#question4").show();
+
+  });
+
+  $("#toQuestion5").click(function(event) {
+    $("#question4").hide();
+    $("#question5").show();
+
+  });
+
   $("#form").submit(function(event) {
 
     $("#beachResult").hide();
@@ -17,56 +41,58 @@ $(document).ready(function() {
     //temple results//
 
     if ( activity === "activityAdventuring" && describe === "describeOffbeat" && (father === "father1" || take === "take3")) {
-      $("#forestTempleResult").show()
+      $("#forestTempleResult").fadeIn("slow")
 
     } else if ( activity === "activityAdventuring" && describe === "describeContent" && (father === "father1" || dream == "dreamShapes" || take === "take3")) {
-      $("#forestTempleResult").show()
+      $("#forestTempleResult").fadeIn("slow")
 
     } else if ( activity === "activityAdventuring" && describe === "describeTired" && (take === "take3" || take === "take1" || father === "father1")) {
-      $("#forestTempleResult").show()
+      $("#forestTempleResult").fadeIn("slow")
 
 
     //beach results//
 
     } else if ( activity === "activityCocktails" && describe === "describeContent" && (father === "father2" || dream === "dreamTraffic")) {
-          $("#beachResult").show()
+          $("#beachResult").fadeIn("slow")
 
     } else if ( activity === "activityCocktails" && describe === "describeTired" && (take === "take1" || dream ==="dreamTraffic")) {
-      $("#beachResult").show()
+      $("#beachResult").fadeIn("slow")
 
     } else if ( activity === "activityAdventuring" && describe === "describeContent" && (take === "take1" || father === "father2" || dream === "dreamTraffic")) {
-      $("#beachResult").show()
+      $("#beachResult").fadeIn("slow")
 
     } else if ( activity === "activityEat" && describe === "describeContent" && (take === "take1" || dream === "dreamTraffic" || father === "father2")) {
-      $("#beachResult").show()
+      $("#beachResult").fadeIn("slow")
 
     } else if ( activity === "activityEat" && describe === "describeTired" && (father === "father2" || dream === "dreamTraffic") && dream !== "dreamVoid" ) {
-      $("#beachResult").show()
+      $("#beachResult").fadeIn("slow")
 
     //apartment results//
 
   } else if ( activity === "activityCocktails" && describe === "describeOffbeat" && (father === "father3" || dream === "dreamVoid")) {
-      $("#apartmentResult").show()
+      $("#apartmentResult").fadeIn("slow")
 
     } else if ( activity === "activityCocktails" && describe === "describeTired" && (dream === "dreamVoid" || take === "take3")) {
-      $("#apartmentResult").show()
+      $("#apartmentResult").fadeIn("slow")
 
     } else if ( activity === "activityEat" && describe === "describeTired" && (dream === "dreamVoid" || father === "father3")) {
-      $("#apartmentResult").show()
+      $("#apartmentResult").fadeIn("slow")
 
     } else if ( activity === "activityAdventure" && describe === "describeTired" && dream === "dreamVoid") {
-      $("#apartmentResult").show()
+      $("#apartmentResult").fadeIn("slow")
 
     } else if ( activity === "activityEat" && describe === "describeContent" && dream === "dreamVoid" && father === "father3") {
-      $("#apartmentResult").show()
+      $("#apartmentResult").fadeIn("slow")
 
 
       //shack results//
 
 
     } else {
-      $("#shackResult").show()
+      $("#shackResult").fadeIn("slow")
       }
+
+    $("#questions").hide();
 
     event.preventDefault();
   });
